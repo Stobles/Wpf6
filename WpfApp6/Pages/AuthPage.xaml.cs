097cb58b296e65eeb875b427e06ac50f51e271b5
Максим Горбачов <ilovespnjensen@inbox.ru>
2022-05-22 00:00:08 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/Pages/AuthPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfApp4.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginBox.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Введите логин и пароль");
+                return;
+            }
+            
+        }
+    }
+}
---
+++ WpfApp6/Pages/PageEmployee.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        private bool isDirty = true;
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void UndoCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Отмена");
+            isDirty = true;
+        }
+
+        private void EditCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+        private void SaveCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !isDirty;
+        }
+
+    }
+}
